debeff5e603f9ace8b9f7837195a91f6cff19e71
IYordanova <ivs.yordanova@gmail.com>
2020-05-28 20:58:03 +0100

    minHeap

---
+++ src/tevalcourse/mergeArrs/MinHeap.java
@@ -0,0 +1,99 @@
+package tevalcourse.mergeArrs;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MinHeap {
+    private final ArrayList<Integer> elements = new ArrayList<>();
+
+    public void add(Integer e) {
+        elements.add(e);
+        int elementIndex = elements.size() - 1;
+        while (!(elementIndex == 0) && !isCorrectChild(elementIndex)) {
+            int parentIndex = parentIndex(elementIndex);
+            swap(elementIndex, parentIndex);
+            elementIndex = parentIndex;
+        }
+    }
+
+    public ArrayList<Integer> popAll() {
+        return IntStream.range(0, elements.size())
+                .mapToObj(num -> popOne())
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public Integer popOne() {
+        if (elements.isEmpty()) {
+            throw new RuntimeException("Empty heap");
+        }
+
+        Integer result = elements.get(0);
+
+        int lastIndex = elements.size() - 1;
+        swap(0, lastIndex);
+        elements.remove(lastIndex);
+
+        int elementIndex = 0;
+        while (!isLeaf(elementIndex) && !isCorrectParent(elementIndex)) {
+            int smallerChildIndex = smallerChildIndex(elementIndex);
+            swap(elementIndex, smallerChildIndex);
+            elementIndex = smallerChildIndex;
+        }
+
+        return result;
+    }
+
+    private int smallerChildIndex(int index) {
+        int leftChildIndex = leftChildIndex(index);
+        int rightChildIndex = rightChildIndex(index);
+
+        if (isInvalidIndex(rightChildIndex)
+                || elements.get(leftChildIndex).compareTo(elements.get(rightChildIndex)) < 0) {
+            return leftChildIndex;
+        }
+
+        return rightChildIndex;
+    }
+
+    private boolean isLeaf(int index) {
+        return isInvalidIndex(leftChildIndex(index));
+    }
+
+    private boolean isCorrectParent(int index) {
+        return isCorrect(index, leftChildIndex(index)) && isCorrect(index, rightChildIndex(index));
+    }
+
+    private boolean isCorrectChild(int index) {
+        return isCorrect(parentIndex(index), index);
+    }
+
+    private boolean isCorrect(int parentIndex, int childIndex) {
+        if (isInvalidIndex(parentIndex) || isInvalidIndex(childIndex)) {
+            return true;
+        }
+        return elements.get(parentIndex).compareTo(elements.get(childIndex)) < 0;
+    }
+
+    private boolean isInvalidIndex(int index) {
+        return index >= elements.size();
+    }
+
+    private void swap(int index1, int index2) {
+        Integer tmp = elements.get(index1);
+        elements.set(index1, elements.get(index2));
+        elements.set(index2, tmp);
+    }
+
+    private int parentIndex(int index) {
+        return (index - 1) / 2;
+    }
+
+    private int leftChildIndex(int index) {
+        return 2 * index + 1;
+    }
+
+    private int rightChildIndex(int index) {
+        return 2 * index + 2;
+    }
+}
---
+++ src/tevalcourse/mergeArrs/MinHeapTester.java
@@ -0,0 +1,44 @@
+package tevalcourse.mergeArrs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MinHeapTester {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> ints1  = new ArrayList<>(Arrays.asList(1, 3));
+        ArrayList<Integer> ints2  = new ArrayList<>(Arrays.asList(2, 4, 5));
+        ArrayList<Integer> ints3  = new ArrayList<>(Arrays.asList(2, 3, 3, 4));
+        ArrayList<ArrayList<Integer>> all  = new ArrayList<>(Arrays.asList(ints1, ints2, ints3));
+        System.out.println(merge(all));
+
+        ArrayList<ArrayList<Integer>> all2  = new ArrayList<>(Arrays.asList(
+                generateArrayList(50),
+                generateArrayList(79),
+                generateArrayList(96)
+        ));
+        System.out.println(merge(all2));
+    }
+
+    private static Random r = new Random(System.nanoTime());
+
+    private static ArrayList<Integer> generateArrayList(int size) {
+        return IntStream.range(0, size)
+                .boxed()
+                .map(num -> r.nextInt(1000))
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+
+    private static ArrayList<Integer> merge(ArrayList<ArrayList<Integer>> arrays){
+        MinHeap heap = new MinHeap();
+        arrays.stream()
+                .flatMap(Collection::stream)
+                .forEach(heap::add);
+        return heap.popAll();
+    }
+}
